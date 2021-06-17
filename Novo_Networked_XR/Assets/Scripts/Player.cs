9818eb4d71fd684c6edf12935e0d9107bc5d6d02
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2021-06-17 17:55:53 +0200

    New Input System Added

---
+++ Novo_Networked_XR/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+
+    [SerializeField] private Vector3 movement = new Vector3();
+
+    [Client]
+    private void Update()
+    {
+
+        if (!hasAuthority)
+        {
+            return;
+        }
+        
+        if (!Input.GetKeyDown(KeyCode.W))
+        {
+            return;
+        }
+        
+        transform.Translate(movement);
+        
+    }
+}
