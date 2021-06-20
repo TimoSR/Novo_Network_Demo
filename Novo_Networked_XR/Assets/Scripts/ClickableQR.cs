7f5116958108f14430d159bd687a993d2d71c752
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2021-06-20 20:45:09 +0200

    Readying for next Phase

--- Novo_Networked_XR/Assets/Scripts/ClickableQR.cs
+++ Novo_Networked_XR/Assets/Scripts/ClickableQR.cs
@@ -9,6 +9,19 @@ public class ClickableQR : NetworkBehaviour
 {
 
     [field: SerializeField] public UnityEvent onClick;
+    private string _objectName;
+    // Belongs somewhere
+    private GameObject _target;
+
+    private void Start()
+    {
+        _objectName = gameObject.name;
+        _objectName = _objectName + "Target";
+        
+        Debug.Log(_objectName);
+        
+        _target = GameObject.Find(_objectName);
+    }
 
     [Server]
     public void OnClick()
