63a18595a2d4d44c0888ca68d164523bd44f11a8
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2021-06-20 19:54:49 +0200

    Data Fetching Working

--- Novo_Networked_XR/Assets/Scripts/PanelOpener.cs
+++ Novo_Networked_XR/Assets/Scripts/PanelOpener.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -16,12 +17,21 @@ public class PanelOpener : MonoBehaviour
     public GameObject crosshair;
     public TMP_Text textArea;
     private StringReader strReader;
+    private string _objectName;
+    private char _firstCharacter;
+
 
     //Data link
     private readonly string getURL = "https://nnedigitaldesignstorage.blob.core.windows.net/candidatetasks/Metadata.csv?sp=r&st=2021-03-15T09:12:39Z&se=2024-11-05T17:12:39Z&spr=https&sv=2020-02-10&sr=b&sig=oyj3Qyg4W42%2BO0d7YqmjxmKk0k%2BLVmE243ixdLaq3gk%3D";
     
     //Panel Activation
-    
+
+    private void Start()
+    {
+        _objectName = gameObject.name;
+        _firstCharacter = _objectName[0];
+    }
+
     public void OpenPanel()
     {
         if (panel && closeButton != null)
@@ -58,9 +68,23 @@ public class PanelOpener : MonoBehaviour
 
                 var text = strReader.ReadToEnd().Split('\n');
                 
+                //Debug.Log(text.Length);
+                
+                //sDebug.Log("Object name: " + _objectName + "First Char: " + _firstCharacter);
+
+                for (int i = 1; i < text.Length-1; i++)
+                {
+                    Debug.Log(i);
+                    
+                    if (text[i][2] == _firstCharacter)
+                    {
+                        textArea.text = text[i];
+                    }
+                }
+
                 //string[] cols = text.Split(';');
 
-                textArea.text = text[1];
+                //textArea.text = text[1][2].ToString();
             }
         }));
     }
